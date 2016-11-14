c4bc7d4df10fb7b155819ba6f1bae82e9b6f8421
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2016-11-14 19:13:57 +0300

    Player: Human, Computer->Dealer

---
+++ src/com/company/Suit.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by falyanguzov on 10.11.2016.
+ */
+public enum Suit {
+    CLUBS, SPADES, HEARTS, DIAMONDS
+}
---
+++ src/com/company/Value.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by falyanguzov on 10.11.2016.
+ */
+public enum Value {
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+    JACK, QUEEN, KING,
+    ACE
+}
